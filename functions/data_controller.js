44863e28c49c262dc5e7e048033e6a501f41d3b5
Bartosz Miłosierny <milosierny.bartosz@gmail.com>
2020-10-02 23:52:50 +0200

    added load data function

---
+++ functions/data_controller.js
@@ -0,0 +1,28 @@
+const fetch = require('node-fetch');
+const perfy = require('perfy');
+
+exports.load = async (count) => {
+    const dataUrl = 'https://jsonplaceholder.typicode.com/posts';
+    let counter = count/100;
+    let data = [];
+
+    console.log(`Starting to get ${count} records from ${dataUrl}...`);
+    perfy.start('data-get');
+
+    while (counter <= count) {
+        const result = await fetch(dataUrl);
+
+        if (result.ok) {
+            const posts = await result.json();
+
+            for (let post of posts) data.push(post);
+
+            counter += 100;
+        }
+        else throw Error(result.statusText);
+    }
+
+    console.log(`Data of ${data.length} records got in ${perfy.end('data-get').time} seconds.`);
+
+    return data;
+}
